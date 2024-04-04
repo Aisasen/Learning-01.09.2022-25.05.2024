14fcb3cdaa2c91423c8194925e48d253d6912ed1
Vladimir Tarasov <109660760+Aisasen@users.noreply.github.com>
2024-04-04 22:59:12 +0600

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+class Worker
+{
+    public string name="Vladimir";
+    public string surname="Tarasov";
+    public int rate=200;
+    public int days=30;
+    public void GetFullName ()
+    {
+        Console.WriteLine (name);
+        Console.WriteLine (surname);
+    }
+    public void GetSalary ()
+    {
+        Console.WriteLine(rate*days);
+    }
+    static void Main()
+    {
+        Console.WriteLine(Worker.GetSalary());
+    }
+}
+
+class MyString
+{
+    public static string Reverse (string text)
+    {
+        char[] chars = text.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+
+    public string UcFirst (string text)
+    {
+        return char.ToUpper(text[0]) + text.Substring(1);
+    }
+
+    public string UcWords (string text)
+    {
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        string titleCase = textInfo.ToTitleCase(text);
+        return titleCase;
+    }
+    static void Main()
+    {
+        Console.WriteLine(MyString.Reverse("Id not Ccecrated"));
+    }
+}
+
